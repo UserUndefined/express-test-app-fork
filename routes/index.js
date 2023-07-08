b20c20758b73b5b0751b75653c539363d9697b36
Chris Ingoe <chris.software.engineer@gmail.com>
2023-07-08 15:31:35 +0100

    Add R1 page

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,9 @@ router.get('/user', function (req, res, next) {
   res.render('user', { title: 'User' });
 });
 
+// R1 page
+router.get('/r1', function (req, res, next) {
+  res.render('r1', { title: 'R1' });
+});
+
 module.exports = router;
